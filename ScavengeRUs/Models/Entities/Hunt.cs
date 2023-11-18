509e81d03115b6fcbdafc4e569f06d9c370b081c
Mahalaa1 <91162858+Mahalaa1@users.noreply.github.com>
2023-11-18 15:34:49 -0500

    Created a working timer. I will need to add to it.

--- ScavengeRUs/Models/Entities/Hunt.cs
+++ ScavengeRUs/Models/Entities/Hunt.cs
@@ -17,7 +17,6 @@ namespace ScavengeRUs.Models.Entities
         }
         public int Id { get; set; }
         public DateTime CreationDate { get; set; }
-
         [DisplayName("Title"), Required]
         public string? HuntName { get; set; }
         [Required]
@@ -27,13 +26,9 @@ namespace ScavengeRUs.Models.Entities
         [DisplayName("Start Date/Time"), Required]
         public DateTime StartDate{ get; set; }
         [DisplayName("End Date/Time"), Required]
-
         public DateTime EndDate { get; set; }
-        
-
         [DisplayName("Status"), Required]
         public string? HuntStatus { get; set; }
-
         [DisplayName("Tasks")]
         public ICollection<HuntLocation> HuntLocations { get; set; } = new List<HuntLocation>();  
         [DisplayName("Access Code")]
